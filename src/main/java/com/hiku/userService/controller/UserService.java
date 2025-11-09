6b00a0a11506e882cbdd2392f40b5bd35f5f6f15
akcabra <andraz.klobucar@gmail.com>
2025-11-09 16:03:49 +0100

    HIKU-13: Added Dockerfile for User service

---
+++ src/main/java/com/hiku/userService/controller/UserService.java
@@ -0,0 +1,8 @@
+package com.hiku.userService.controller;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/user")
+public class UserService extends Application {
+}
