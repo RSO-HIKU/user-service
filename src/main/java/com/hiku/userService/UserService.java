94f17395c66ed2cd42b5a6c1fe3b7981af6af11e
akcabra <andraz.klobucar@gmail.com>
2026-01-03 21:12:03 +0100

    HIKU-31: Urejeno za CICD in dodan Keycloak

--- src/main/java/com/hiku/userService/UserService.java
+++ src/main/java/com/hiku/userService/UserService.java
@@ -1,7 +1,10 @@
 package com.hiku.userService;
 
+import javax.annotation.security.DeclareRoles;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+
+import org.eclipse.microprofile.auth.LoginConfig;
 
 import com.hiku.userService.controller.Hello;
 import com.hiku.userService.controller.UserController;
@@ -13,6 +16,8 @@ import java.util.Set;
  * Explicit JAX-RS application class that registers resources and providers.
  * Some JAX-RS runtimes require explicit registration instead of classpath scanning.
  */
+@LoginConfig(authMethod = "MP-JWT")
+@DeclareRoles({"user", "admin"})
 @ApplicationPath("/api/user")
 public class UserService extends Application {
 
