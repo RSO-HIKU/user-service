11e10b17575ddc37908aa55d7c9cec2e99283d66
akcabra <andraz.klobucar@gmail.com>
2026-01-10 22:52:24 +0100

    Popravki

--- src/main/java/com/hiku/userService/messaging/RabbitMQConfig.java
+++ src/main/java/com/hiku/userService/messaging/RabbitMQConfig.java
@@ -15,8 +15,8 @@ public class RabbitMQConfig {
             ConnectionFactory factory = new ConnectionFactory();
             factory.setHost(System.getenv().getOrDefault("RABBITMQ_HOST", "rabbitmq.platform.svc.cluster.local"));
             factory.setPort(Integer.parseInt(System.getenv().getOrDefault("RABBITMQ_PORT", "5672")));
-            factory.setUsername(System.getenv().getOrDefault("RABBITMQ_USER", "hikuuser"));
-            factory.setPassword(System.getenv().getOrDefault("RABBITMQ_PASSWORD", "hikupassword"));
+            factory.setUsername(System.getenv().getOrDefault("RABBITMQ_DEFAULT_USER", "hikuuser"));
+            factory.setPassword(System.getenv().getOrDefault("RABBITMQ_DEFAULT_PASS", "hikupassword"));
             
             connection = factory.newConnection();
             channel = connection.createChannel();
